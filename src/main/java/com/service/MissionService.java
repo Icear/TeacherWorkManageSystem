176b35378903e9ee37d011b666ab93cc8d28c914
oooooreo <Wchoward@users.noreply.github.com>
2018-06-19 10:42:24 +0800

    加入了MissionService接口，对任务的增删改关闭和三种id查找，TODO：关闭接口有待商榷

---
+++ src/main/java/com/service/MissionService.java
@@ -0,0 +1,66 @@
+package com.service;
+
+import com.entity.MissionEntity;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
+
+public interface MissionService {
+    /**
+     * 管理员创建指定类型任务
+     *
+     * @param mission mission对象
+     * @return 成功返回新的mission实体，失败返回null
+     */
+    @NotNull Optional<MissionEntity> addMission(@NotNull MissionEntity mission);
+
+    /**
+     * 修改指定任务信息
+     *
+     * @param mission mission中需带有旧的任务信息的id
+     * @return 成功返回新的mission实体
+     */
+    @NotNull Optional<MissionEntity> updateMission(@NotNull MissionEntity mission);
+
+    /**
+     * 根据id删除mission
+     *
+     * @param mission 有效的含id的mission类
+     * @return 是否删除成功
+     */
+    boolean deleteMission(@NotNull MissionEntity mission);
+
+    // TODO: 2018/6/19 关闭mission应该是修改状态吧，然后把updatable=false？这种？所以会返回实体吧？
+
+    /**
+     * 关闭指定任务
+     *
+     * @param mission 有效的含id的mission类
+     * @return 返回新的mission实体
+     */
+    @NotNull Optional<MissionEntity> closeMission(@NotNull MissionEntity mission);
+
+    /**
+     * 根据missionId查找mission
+     *
+     * @param missionId missionId
+     * @return mission对象
+     */
+    @NotNull Optional<MissionEntity> findMissionByMissionId(int missionId);
+
+    /**
+     * 根据文件mission id查找mission
+     *
+     * @param fileMissionId fileMissionId
+     * @return mission对象
+     */
+    @NotNull Optional<MissionEntity> findMissionByFileMissionId(int fileMissionId);
+
+    /**
+     * 根据回复mission id查找mission
+     *
+     * @param replyMissionId replyMissionId
+     * @return mission对象
+     */
+    @NotNull Optional<MissionEntity> findMissionByReplyMissionId(int replyMissionId);
+}
