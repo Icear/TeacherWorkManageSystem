05a628309a50224c1cf2b10c912a965f519f5c2c
oooooreo <Wchoward@users.noreply.github.com>
2018-06-20 23:13:46 +0800

    EMSService实现类

---
+++ src/main/java/com/service/impl/EMSServiceImpl.java
@@ -0,0 +1,18 @@
+package com.service.impl;
+
+import com.service.EMSService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class EMSServiceImpl implements EMSService {
+    private static Logger logger = LogManager.getLogger(EMSServiceImpl.class);
+
+    @Override
+    public void sendEMS(String emsContent) {
+        logger.debug(emsContent);
+    }
+}
