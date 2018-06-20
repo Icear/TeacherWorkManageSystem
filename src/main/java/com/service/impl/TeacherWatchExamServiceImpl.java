a076e7d041000554fd01d5f5d0533035188a75d2
oooooreo <Wchoward@users.noreply.github.com>
2018-06-21 00:28:04 +0800

    TeacherService实现类更新，完成之前未完成的增删改查操作

--- src/main/java/com/service/impl/TeacherWatchExamServiceImpl.java
+++ src/main/java/com/service/impl/TeacherWatchExamServiceImpl.java
@@ -1,8 +1,10 @@
 package com.service.impl;
 
+import com.dao.TeacherWatchExamEntityDao;
 import com.entity.TeacherWatchExamEntity;
 import com.service.TeacherWatchExamService;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,24 +14,30 @@ import java.util.Optional;
 @Service
 @Transactional
 public class TeacherWatchExamServiceImpl implements TeacherWatchExamService {
+    private final TeacherWatchExamEntityDao teacherWatchExamEntityDao;
+
+    @Autowired
+    public TeacherWatchExamServiceImpl(TeacherWatchExamEntityDao teacherWatchExamEntityDao) {
+        this.teacherWatchExamEntityDao = teacherWatchExamEntityDao;
+    }
 
     @Override
     public @NotNull TeacherWatchExamEntity addTeacherWatchExam(@NotNull TeacherWatchExamEntity teacherWatchExamEntity) {
-        return null;
+        return teacherWatchExamEntityDao.add(teacherWatchExamEntity);
     }
 
     @Override
     public void deleteTeacherWatchExam(@NotNull TeacherWatchExamEntity teacherWatchExamEntity) {
-
+        teacherWatchExamEntityDao.delete(teacherWatchExamEntity);
     }
 
     @Override
     public @NotNull Optional<TeacherWatchExamEntity> updateTeacherWatchExam(@NotNull TeacherWatchExamEntity teacherWatchExamEntity) {
-        return Optional.empty();
+        return Optional.ofNullable(teacherWatchExamEntityDao.edit(teacherWatchExamEntity));
     }
 
     @Override
     public @NotNull List<TeacherWatchExamEntity> findTeacherWatchExams() {
-        return null;
+        return teacherWatchExamEntityDao.list();
     }
 }
