0865a2a2427e9691be8379d0d4e0c55dee3c1f61
Icear <Icear@users.noreply.github.com>
2018-06-22 23:28:16 +0800

    重新配置deploy脚本

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,5 +2,5 @@
 
 echo $PWD
 
-scp -i $TRAVIS_BUILD_DIR/id_rsa -P $PORT -o StrictHostKeyChecking=no$TRAVIS_BUILD_DIR/target/ROOT.war root@project.oooooreo.xyz:/root/docker-TMS/webapps/
-ssh -i $TRAVIS_BUILD_DIR/id_rsa -p $PORT root@project.oooooreo "/root/docker-TMS/depoloy-docker-TMS.sh"
+scp -i $TRAVIS_BUILD_DIR/id_rsa -P $PORT -o 'StrictHostKeyChecking=no' $TRAVIS_BUILD_DIR/target/ROOT.war root@project.oooooreo.xyz:/root/docker-TMS/webapps/
+ssh -i $TRAVIS_BUILD_DIR/id_rsa -p $PORT root@project.oooooreo.xyz "/root/docker-TMS/depoloy-docker-TMS.sh"
