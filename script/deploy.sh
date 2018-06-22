20fcfd377b30a5423602e19171d4fa11672ca7c1
Icear <Icear@users.noreply.github.com>
2018-06-22 21:49:30 +0800

    重新配置deploy脚本

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-echo $SECRET_KEY > id_rsa
-chmod 600 id_rsa
 scp -i id_rsa -P $PORT ./target/ROOT.war $USER@$SERVER:$DEPLOY_FILE_LOCATION
 ssh -i id_rsa $USER@$SERVER $DEPLOY_SCRIPT_LOCATION
