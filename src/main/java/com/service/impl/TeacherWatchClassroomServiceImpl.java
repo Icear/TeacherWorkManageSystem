ada456def84b204bbb225f89c94b5a0dec2e988e
oooooreo <Wchoward@users.noreply.github.com>
2018-06-22 10:09:51 +0800

    将teacherWatchExam改为watchClassroom

---
+++ src/main/java/com/service/impl/TeacherWatchClassroomServiceImpl.java
@@ -0,0 +1,43 @@
+package com.service.impl;
+
+import com.dao.TeacherWatchClassroomEntityDao;
+import com.entity.TeacherWatchClassroomEntity;
+import com.service.TeacherWatchClassroomService;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class TeacherWatchClassroomServiceImpl implements TeacherWatchClassroomService {
+    private final TeacherWatchClassroomEntityDao teacherWatchClassroomEntityDao;
+
+    @Autowired
+    public TeacherWatchClassroomServiceImpl(TeacherWatchClassroomEntityDao teacherWatchClassroomEntityDao) {
+        this.teacherWatchClassroomEntityDao = teacherWatchClassroomEntityDao;
+    }
+
+    @Override
+    public @NotNull TeacherWatchClassroomEntity addTeacherWatchClassroom(@NotNull TeacherWatchClassroomEntity teacherWatchClassroomEntity) {
+        return teacherWatchClassroomEntityDao.add(teacherWatchClassroomEntity);
+    }
+
+    @Override
+    public void deleteTeacherWatchClassroom(@NotNull TeacherWatchClassroomEntity teacherWatchClassroomEntity) {
+        teacherWatchClassroomEntityDao.delete(teacherWatchClassroomEntity);
+    }
+
+    @Override
+    public @NotNull Optional<TeacherWatchClassroomEntity> updateTeacherWatchClassroom(@NotNull TeacherWatchClassroomEntity teacherWatchClassroomEntity) {
+        return Optional.ofNullable(teacherWatchClassroomEntityDao.edit(teacherWatchClassroomEntity));
+    }
+
+    @Override
+    public @NotNull List<TeacherWatchClassroomEntity> findTeacherWatchClassrooms() {
+        return teacherWatchClassroomEntityDao.list();
+    }
+}
