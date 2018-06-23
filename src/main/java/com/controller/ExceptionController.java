9d11965d0a2891fa082ac3688f4224ef24da31d3
Coco-In-Sky <Coco-In-Sky@users.noreply.github.com>
2018-06-23 09:32:51 +0800

    NoResultException的统一异常处理 老师写的异常有warning

--- src/main/java/com/controller/ExceptionController.java
+++ src/main/java/com/controller/ExceptionController.java
@@ -1,10 +1,15 @@
 package com.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.persistence.NoResultException;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
@@ -13,13 +18,14 @@ import java.util.Set;
 @ControllerAdvice
 public class ExceptionController {
 
+    private static Logger logger = LogManager.getLogger(ExceptionController.class);
     /**
      * 处理对象属性校验失败异常
      *
-     * @param e
-     * @param request
-     * @param redirectAttributes
-     * @return
+     * @param e 异常
+     * @param request http请求
+     * @param redirectAttributes 重定向属性
+     * @return 重定向的地址
      */
     @ExceptionHandler(BindException.class)
     public String getBindException(
@@ -47,4 +53,16 @@ public class ExceptionController {
         redirectAttributes.addFlashAttribute("exception", strBuilder.toString());
         return "redirect:" + request.getHeader("referer");
     }
+
+    /**
+     * 没有找到对应的实体
+     *
+     * @param e 异常
+     */
+    @ExceptionHandler(NoResultException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public void getNoResultException(NoResultException e) {
+        logger.debug("未找到对应的实体");
+    }
+
 }
