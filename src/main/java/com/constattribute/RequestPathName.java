b813e89ffb999f2979b026ba620625e662daa3fe
Icear <Icear@users.noreply.github.com>
2018-06-30 17:58:17 +0800

    修改ClassRoom接口名

--- src/main/java/com/constattribute/RequestPathName.java
+++ src/main/java/com/constattribute/RequestPathName.java
@@ -3,5 +3,5 @@ package com.constattribute;
 public class RequestPathName {
     public static final String AUTHORIZE = "/authorize";
     public static final String TEACHERS = "/teachers";
-    public static final String CLASS_ROOMS = "/classrooms";
+    public static final String CLASSROOMS = "/classrooms";
 }
